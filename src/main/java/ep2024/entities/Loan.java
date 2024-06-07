5d642e8d587bd2ea4bd43ce93f0943a1d3e1edeb
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-06-07 12:21:50 +0200

    adds user and loan classes

--- src/main/java/ep2024/entities/Loan.java
+++ src/main/java/ep2024/entities/Loan.java
@@ -1,4 +1,82 @@
 package ep2024.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "loans")
 public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private User user;
+    @Column(name = "loaned_items")
+    private List<Catalogue> items;
+    @Column(name = "loan_start_date")
+    private LocalDate loanStartDate;
+    @Column(name = "expected_return_date")
+    private LocalDate expectedReturnDate;
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    public Loan() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Catalogue> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Catalogue> items) {
+        this.items = items;
+    }
+
+    public LocalDate getLoanStartDate() {
+        return loanStartDate;
+    }
+
+    public void setLoanStartDate(LocalDate loanStartDate) {
+        this.loanStartDate = loanStartDate;
+    }
+
+    public LocalDate getExpectedReturnDate() {
+        return expectedReturnDate;
+    }
+
+    public void setExpectedReturnDate(LocalDate expectedReturnDate) {
+        this.expectedReturnDate = expectedReturnDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "id=" + id +
+                ", user=" + user +
+                ", items=" + items +
+                ", loanStartDate=" + loanStartDate +
+                ", expectedReturnDate=" + expectedReturnDate +
+                ", returnDate=" + returnDate +
+                '}';
+    }
 }
--- src/main/java/ep2024/entities/User.java
+++ src/main/java/ep2024/entities/User.java
@@ -1,4 +1,67 @@
 package ep2024.entities;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "membership_number")
+    private UUID membershipNumber;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "date_of_birth")
+    private String dateOfBirth;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public UUID getMembershipNumber() {
+        return membershipNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "membershipNumber=" + membershipNumber +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
 }
