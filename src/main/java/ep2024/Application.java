01851236a882244b682abb9d682ffe369fc45aa9
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-06-07 13:16:06 +0200

    adds elementFactory

--- src/main/java/ep2024/Application.java
+++ src/main/java/ep2024/Application.java
@@ -1,19 +1,25 @@
 package ep2024;
 
-import com.github.javafaker.Faker;
+import ep2024.entities.Catalogue;
+import ep2024.entities.ElementFactory;
+import ep2024.entities.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-import java.util.Random;
+import java.util.List;
 
 public class Application {
     public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4-w3-d5");
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
-        Faker faker = new Faker();
-        Random random = new Random();
+
+        List<Catalogue> libraryCatalogue = ElementFactory.generateLibraryCatalogue();
+        List<User> users = ElementFactory.generateUsers();
+
+        System.out.println(libraryCatalogue);
+        System.out.println(users);
 
         System.out.println("Hello World!");
     }
---
+++ src/main/java/ep2024/entities/ElementFactory.java
@@ -0,0 +1,64 @@
+package ep2024.entities;
+
+import com.github.javafaker.Faker;
+import ep2024.enums.Release;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElementFactory {
+    private static Faker faker = new Faker();
+
+    public static List<Catalogue> generateLibraryCatalogue() {
+        List<Catalogue> libraryCatalogue = new ArrayList<>();
+        libraryCatalogue.addAll(generateBooks());
+        libraryCatalogue.addAll(generateArticles());
+        return libraryCatalogue;
+    }
+
+    public static List<Book> generateBooks() {
+        List<Book> books = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            String title = faker.book().title();
+            int year = faker.number().numberBetween(1900, 2024);
+            int pages = faker.number().numberBetween(100, 900);
+            String author = faker.book().author();
+            String genre = faker.book().genre();
+
+            Book book = new Book(title, year, pages, author, genre);
+            books.add(book);
+        }
+        return books;
+    }
+
+    public static List<Article> generateArticles() {
+        List<Article> articles = new ArrayList<>();
+        Release[] releases = Release.values();
+
+        for (int i = 0; i < 10; i++) {
+            String title = faker.book().title();
+            int year = faker.number().numberBetween(1900, 2024);
+            int pages = faker.number().numberBetween(30, 250);
+            Release release = releases[faker.random().nextInt(releases.length)];
+
+            Article article = new Article(title, year, pages, release);
+            articles.add(article);
+        }
+        return articles;
+    }
+
+    public static List<User> generateUsers() {
+        List<User> users = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String dateOfBirth = faker.date().birthday(18, 85).toString();
+
+            User user = new User(firstName, lastName, dateOfBirth);
+            users.add(user);
+        }
+        return users;
+    }
+}
